8a23c6566b03907cd820dcc61a5efa4fc0741f64
Patrik Rosecky <prosecky@redhat.com>
2023-03-14 15:41:06 +0100

    docs sssctl.py

--- src/tests/system/lib/sssd/utils/sssctl.py
+++ src/tests/system/lib/sssd/utils/sssctl.py
@@ -1,4 +1,3 @@
-from typing import List
 
 from pytest_mh import MultihostHost, MultihostUtility
 from pytest_mh._private.multihost import MultihostRole
@@ -6,10 +5,27 @@ from pytest_mh.ssh import SSHLog, SSHProcess, SSHProcessResult
 
 
 class SSSCTLUtils(MultihostUtility[MultihostHost]):
+    """
+    Manage and configure cache.
 
+    """
     def __init__(self, host: MultihostHost ) -> None:
+        """
+        :param host: Multihost host.
+        :type host: MultihostHost
+        """ """"""
         super().__init__(host)
-        self.host = host
 
-    def cache_expire(self, options: List[str]=[]) -> SSHProcessResult:
-        return self.host.ssh.exec(['sssctl', 'cache-expire'] + options)
+    def cache_expire(self, args: list[str]=None) -> None:
+        """
+        Run ``sssctl cache-expire`` command.
+
+        :param args: Additional arguments, defaults to empty list.
+        :type args: list[str], optional
+        """
+        if args is None:
+            args = []
+        self.host.ssh.exec(['sssctl', 'cache-expire', *args])
+
+
+    
